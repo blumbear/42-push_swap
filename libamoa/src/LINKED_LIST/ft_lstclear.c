7e5752a1ea06e26f5d5c6ed43f2b52ef587c9a64
Tom Taquet <ttaquet@c1r3p2.42lehavre.fr>
2024-05-07 15:56:09 +0200

    + | add custom linked list function to use th programme

---
+++ libamoa/src/LINKED_LIST/ft_lstclear.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttaquet <ttaquet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/21 16:40:41 by ttaquet           #+#    #+#             */
+/*   Updated: 2024/05/07 15:16:19 by ttaquet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libamoa.h"
+
+void			ft_lstclear(t_list **list, void (*del_fun)(int))
+{
+	t_list	*tmp;
+
+	while (*list)
+	{
+		tmp = (*list)->next;
+		ft_lstdelone(*list, del_fun);
+		*list = tmp;
+	}
+	list = NULL;
+}
